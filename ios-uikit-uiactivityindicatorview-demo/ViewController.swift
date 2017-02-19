a730555e9c67d09703a2260c7d117d95436c4594
taguchi-k <k.taguchi@st-ventures.jp>
2017-02-19 23:13:33 +0900

    Add SampleCode

--- ios-uikit-uiactivityindicatorview-demo/ViewController.swift
+++ ios-uikit-uiactivityindicatorview-demo/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet private weak var whiteLargeActivityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet private weak var whiteActivityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet private weak var grayActivityIndicatorView: UIActivityIndicatorView!
+    
+    @IBAction private func toggleHidesWhenStopped(_ sender: Any) {
+        /*
+         インジケーターのアニメーションが止まったときにhiddenにするかどうかを設定する
+         （storyboardで設定可能）
+         */
+        whiteLargeActivityIndicatorView.hidesWhenStopped = !whiteLargeActivityIndicatorView.hidesWhenStopped
+        whiteActivityIndicatorView.hidesWhenStopped = !whiteActivityIndicatorView.hidesWhenStopped
+        grayActivityIndicatorView.hidesWhenStopped = !grayActivityIndicatorView.hidesWhenStopped
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction private func activityIndicatorAction(_ sender: Any) {
+        /*
+         ActivityIndicatoがアニメーション中か判定
+         （isAnimatingはstoryboardで設定可能
+         　チェックを付けるとアニメーションしている状態で表示される）
+         */
+        if whiteLargeActivityIndicatorView.isAnimating {
+            // アニメーションストップ
+            whiteLargeActivityIndicatorView.stopAnimating()
+        } else {
+            // アニメーションスタート
+            whiteLargeActivityIndicatorView.startAnimating()
+        }
+        
+        if whiteActivityIndicatorView.isAnimating {
+            whiteActivityIndicatorView.stopAnimating()
+        } else {
+            whiteActivityIndicatorView.startAnimating()
+        }
+        
+        if grayActivityIndicatorView.isAnimating {
+            grayActivityIndicatorView.stopAnimating()
+        } else {
+            grayActivityIndicatorView.startAnimating()
+        }
     }
-
-
+    
 }
 
